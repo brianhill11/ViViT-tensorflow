316ffba392ef91791742c2a9765daad3f59725f6
Brian Hill <brian.l.hill11@gmail.com>
2021-06-30 18:41:52 -0700

    Initial version of keras ViViT implementation ported from pytorch
    Initial code for keras/tensorflow implementation of the ViViT model architecture, ported from pytorch

---
+++ module.py
@@ -0,0 +1,182 @@
+import tensorflow as tf
+from tensorflow import nn, einsum
+from tensorflow.keras.layers import Dense, Dropout, ReLU
+# import torch.nn.functional as F
+import numpy as np
+
+from einops import rearrange, repeat
+from einops.layers.tensorflow import Rearrange
+
+class Residual(tf.keras.layers.Layer):
+    def __init__(self, fn):
+        super(Residual, self).__init__()
+        self.fn = fn
+    def call(self, x, **kwargs):
+        return self.fn(x, **kwargs) + x
+
+class PreNorm(tf.keras.layers.Layer):
+    def __init__(self, dim, fn):
+        super(PreNorm, self).__init__()
+        self.norm = tf.keras.layers.LayerNormalization()
+        self.fn = fn
+    def call(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class FeedForward(tf.keras.layers.Layer):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super(FeedForward, self).__init__()
+        self.net = tf.keras.Sequential([
+            Dense(hidden_dim),
+            ReLU(),
+            Dropout(dropout),
+            Dense(dim),
+            Dropout(dropout)
+        ])
+    def call(self, x):
+        return self.net(x)
+
+class Attention(tf.keras.layers.Layer):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super(Attention, self).__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.to_qkv = Dense(inner_dim * 3, use_bias = False)
+
+        self.to_out = tf.keras.Sequential([
+            Dense(dim),
+            Dropout(dropout)
+        ]) if project_out else nn.Identity()
+
+    def call(self, x):
+        b, n, _, h = *x.shape, self.heads
+        qkv = tf.split(self.to_qkv(x), num_or_size_splits=3, axis=-1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+
+        attn = tf.nn.softmax(dots, axis=-1)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out =  self.to_out(out)
+        return out
+
+
+class ReAttention(tf.keras.layers.Layer):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super(ReAttention, self).__init__()
+        inner_dim = dim_head *  heads
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.to_qkv = Dense(inner_dim * 3, use_bias = False)
+
+        self.reattn_weights = tf.Variable(np.random.randn(heads, heads))
+
+        self.reattn_norm = tf.keras.Sequential([
+            Rearrange('b h i j -> b i j h'),
+            tf.keras.layers.LayerNormalization(),
+            Rearrange('b i j h -> b h i j')
+        ])
+
+        self.to_out = tf.keras.Sequential([
+            Dense(dim),
+            Dropout(dropout)
+        ])
+
+    def call(self, x):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+
+        # attention
+
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+        attn = dots.softmax(dim=-1)
+
+        # re-attention
+
+        attn = einsum('b h i j, h g -> b g i j', attn, self.reattn_weights)
+        attn = self.reattn_norm(attn)
+
+        # aggregate and out
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out =  self.to_out(out)
+        return out
+    
+class LeFF(tf.keras.layers.Layer):
+    
+    def __init__(self, dim = 192, scale = 4, depth_kernel = 3):
+        super(LeFF, self).__init__()
+        
+        scale_dim = dim*scale
+        self.up_proj = tf.keras.Sequential([
+                                    Dense(scale_dim),
+                                    Rearrange('b n c -> b c n'),
+                                    nn.BatchNorm1d(scale_dim),
+                                    ReLU(),
+                                    Rearrange('b c (h w) -> b c h w', h=14, w=14)
+        ])
+        
+        self.depth_conv =  tf.keras.Sequential([nn.Conv2d(scale_dim, scale_dim, kernel_size=depth_kernel, padding=1, groups=scale_dim, bias=False),
+                          nn.BatchNorm2d(scale_dim),
+                          ReLU(),
+                          Rearrange('b c h w -> b (h w) c', h=14, w=14)
+        ])
+        
+        self.down_proj = tf.keras.Sequential([
+                                    Dense(dim),
+                                    Rearrange('b n c -> b c n'),
+                                    nn.BatchNorm1d(dim),
+                                    ReLU(),
+                                    Rearrange('b c n -> b n c')
+        ])
+        
+    def call(self, x):
+        x = self.up_proj(x)
+        x = self.depth_conv(x)
+        x = self.down_proj(x)
+        return x
+    
+    
+class LCAttention(tf.keras.layers.Layer):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super(LCAttention, self).__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.to_qkv = Dense(inner_dim * 3, use_bias = False)
+
+        self.to_out = tf.keras.Sequential([
+            Dense(dim),
+            Dropout(dropout)
+        ]) if project_out else nn.Identity()
+
+    def call(self, x):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+        q = q[:, :, -1, :].unsqueeze(2) # Only Lth element use as query
+
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+
+        attn = dots.softmax(dim=-1)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out =  self.to_out(out)
+        return out
+        
+        
+        
+        
+
---
+++ vivit.py
@@ -0,0 +1,100 @@
+# %%
+import tensorflow as tf
+from tensorflow import nn, einsum
+from tensorflow.keras.layers import Dense, Dropout, Input, LayerNormalization
+# import torch.nn.functional as F
+from einops import rearrange, repeat
+from einops.layers.tensorflow import Rearrange
+from module import Attention, PreNorm, FeedForward
+import numpy as np
+
+class Transformer(tf.keras.layers.Layer):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+        super(Transformer, self).__init__()
+        self.layers = []
+        self.norm = LayerNormalization()
+        for _ in range(depth):
+            self.layers.append([
+                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+            ])
+
+    def call(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return self.norm(x)
+
+
+  
+class ViViT(tf.keras.Model):
+    def __init__(self, image_size, patch_size, num_classes, num_frames, dim = 192, depth = 4, heads = 3, pool = 'cls', in_channels = 3, dim_head = 64, dropout = 0.,
+                 emb_dropout = 0., scale_dim = 4, ):
+        super(ViViT, self).__init__()
+        
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+
+        assert image_size % patch_size == 0, 'Image dimensions must be divisible by the patch size.'
+        num_patches = (image_size // patch_size) ** 2
+        patch_dim = in_channels * patch_size ** 2
+        self.to_patch_embedding = tf.keras.Sequential([
+            Input(shape=(num_frames, in_channels, image_size, image_size)),
+            Rearrange('b t c (h p1) (w p2) -> b t (h w) (p1 p2 c)', p1 = patch_size, p2 = patch_size),
+            Dense(dim),
+        ])
+
+        self.pos_embedding = self.add_weight("pos_embedding", shape=(1, num_frames, num_patches + 1, dim))
+        self.space_token = self.add_weight("space_token", shape=(1, 1, dim))
+        self.space_transformer = Transformer(dim, depth, heads, dim_head, dim*scale_dim, dropout)
+
+        self.temporal_token = self.add_weight("temp_token", shape=(1, 1, dim))
+        self.temporal_transformer = Transformer(dim, depth, heads, dim_head, dim*scale_dim, dropout)
+
+        self.dropout = Dropout(emb_dropout)
+        self.pool = pool
+
+        self.mlp_head = tf.keras.Sequential([
+            LayerNormalization(),
+            Dense(num_classes)
+        ])
+
+    def call(self, x):
+        x = self.to_patch_embedding(x)
+        print("x shape:", x.shape)
+        b, t, n, _ = x.shape
+
+        cls_space_tokens = repeat(self.space_token, '() n d -> b t n d', b = b, t=t)
+        x = tf.concat((cls_space_tokens, x), axis=2)
+        print("now x:", x.shape)
+        x += self.pos_embedding[:, :, :(n + 1)]
+        x = self.dropout(x)
+
+        x = rearrange(x, 'b t n d -> (b t) n d')
+        x = self.space_transformer(x)
+        x = rearrange(x[:, 0], '(b t) ... -> b t ...', b=b)
+
+        cls_temporal_tokens = repeat(self.temporal_token, '() n d -> b n d', b=b)
+        x = tf.concat((cls_temporal_tokens, x), axis=1)
+
+        x = self.temporal_transformer(x)
+        
+
+        x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
+
+        return self.mlp_head(x)
+    
+    
+    
+# %%
+if __name__ == "__main__":
+    
+    X = np.ones(shape=(1, 10, 6, 36, 36), dtype=np.float64)
+    y = np.ones(shape=(1, 10, 2), dtype=np.float64)
+    
+    model = ViViT(image_size=36, patch_size=12, in_channels=6, num_classes=2, num_frames=10, dim=13)
+    tf.keras.utils.plot_model(model, show_shapes=False,)
+    model.compile(optimizer=tf.keras.optimizers.Adam(), loss="mse")
+    model.build(input_shape=(1, 10, 6, 36, 36))
+    print(model.summary())
+    model = model.fit(X, y, batch_size=1)
